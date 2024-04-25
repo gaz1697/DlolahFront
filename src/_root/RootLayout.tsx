b04512bcdb7fdb19c7493212dc7e4afebe13c13b
gaz1697 <gazlan111@gmail.com>
2024-04-25 16:04:30 +0300

    add json icon

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,5 +1,5 @@
 import NavBar from '@/NavBar';
-import voiceInputAnimation from '/Users/abdulrahman/Documents/Projects/Dlolah/Animation - 1711499666786.json'
+import voiceInputAnimation from '../../Animation - 1711499666786.json'
 import Lottie from 'lottie-react'
 const RootLayout = () => {
   var x = <Lottie animationData={voiceInputAnimation} loop={true}/>;
